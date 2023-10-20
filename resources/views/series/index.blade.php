3167b5f2055de38c5421036d23246358727d4e3f
FelipePy <felipespsampaiomotasp@gmail.com>
2023-10-20 11:34:39 -0300

    Permissions of the user

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -4,23 +4,25 @@
     <ul class="list-group">
         @foreach ($series as $serie)
             <li class="list-group-item justify-content-between d-flex align-items-center">
-                <a class="link-dark" href='{{ route('seasons.index', $serie->id) }}'>
+                @auth<a class="link-dark" href='{{ route('seasons.index', $serie->id) }}'>@endauth
                     {{ $serie->name }}
-                </a>
+                @auth</a>@endauth
 
-                <span class="d-flex">
-                    <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
-                        E
-                    </a>
+                @auth
+                    <span class="d-flex">
+                        <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
+                            E
+                        </a>
 
-                    <form action="{{ route('series.destroy', $serie->id) }}" method="post" class="ms-2">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger btn-sm">
-                            X
-                        </button>
-                    </form>
-            </span>
+                        <form action="{{ route('series.destroy', $serie->id) }}" method="post" class="ms-2">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger btn-sm">
+                                X
+                            </button>
+                        </form>
+                    </span>
+                @endauth
 
             </li>
         @endforeach
